d91b83b7f991bc27ad0b7293f05e2a619f47319f
nicolas <nicolas.hoog@gmail.com>
2019-11-16 15:48:48 -0500

    create linked-list class

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([-10980, 3, -4, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+const sortArrayOfNumber = new Sorter(numbersCollection);
+sortArrayOfNumber.sort();
 
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Xaayb');
+const sortString = new Sorter(charactersCollection);
+sortString.sort();
+
+console.log(charactersCollection.data);
